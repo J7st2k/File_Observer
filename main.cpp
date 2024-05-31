666625556f62a7e08734ed167ffff1c391a4d108
J7st2k <sav-n-an@mail.ru>
2024-05-31 17:16:46 +0700

    Вынес подсчёт процетов как отдельную функцию в main'e.

--- main.cpp
+++ main.cpp
@@ -15,6 +15,69 @@ void printMap(const QMap<QString, int> map) {
     }
 }
 
+void printMap(QMap<QString, QString>* map) {
+    QTextStream cout(stdout);
+    if(!map) return;
+    if(!map->empty()) {
+        QMapIterator<QString, QString> i(*map);
+        while(i.hasNext()) {
+            i.next();
+            cout << i.key() << QString(": ") << i.value() << Qt::endl;
+        }
+    }
+}
+
+QMap<QString, QString>* countPercent(const QMap<QString, int>& memory, int type, float border = 0.01) {
+    float total = 0;
+    float tmp = 0;
+    bool flag = false;
+    QMap<QString, QString>* map = new QMap<QString, QString>;
+    if(!map) return map;
+    if(memory.empty()) return map;
+    if(border >= 100) border = 0;
+    QMapIterator<QString, int> i(memory);
+    while(i.hasNext()) {
+        i.next();
+        total += i.value();
+    }
+    i.toFront();
+    if(type == 2) {
+        if(total == 0) {
+            while(i.hasNext()) {
+                i.next();
+                map->insert(QString("*.") + i.key(), QString("0.00 \%"));
+            }
+            return map;
+        }
+        float othr = 0;
+        while(i.hasNext()) {
+            i.next();
+            tmp = float(i.value())/total;
+            if (tmp < border) {
+                othr += tmp;
+                flag = true;
+            }
+            else map->insert(QString("*.") + i.key(), QString::number(tmp*100, 'f', 2) + QString(" \%"));
+        }
+        if (flag)
+            map->insert(QString("Others"), QString::number(othr*100, 'f', 2) + QString(" \%"));
+    } else if (type == 1) {
+        if(total == 0) {
+            map->insert("*Current path*", QString("0.00 \%"));
+            return map;
+        }
+        while(i.hasNext()) {
+            i.next();
+            tmp = float(i.value())/total;
+            if (tmp < border)
+                map->insert(i.key(), QString("<%1 \%").arg(QString::number(border)));
+            else map->insert(i.key(), QString("%1 \%").arg(QString::number(tmp*100, 'f', 2)));
+        }
+    }
+    return map;
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -22,14 +85,24 @@ int main(int argc, char *argv[])
     QDir myDir;
     QString info;
     //info = myDir.path();
-    info = QString("D:\\Games");
-    std::cout << "Folders:\n";
+    info = QString("D:\\Games\\EmptyFolder");
+
+
     GetStatistics g(std::make_shared<fileStatistics>());
     g.FillMap(info);
-    printMap(g.GetMap());
-    std::cout << "Formats:\n";
+    // std::cout << "Folders raw:\n";
+    // printMap(g.GetMap());
+    std::cout << "Folders percent:\n";
+    printMap(countPercent(g.GetMap(), 1));
+
+
     g.setStrategy(std::make_shared<formatStatistics>());
     g.FillMap(info);
-    printMap(g.GetMap());
+    // std::cout << "Formats raw:\n";
+    // printMap(g.GetMap());
+    std::cout << "Formats percent:\n";
+    printMap(countPercent(g.GetMap(), 2));
+
+
     return a.exec();
 }
